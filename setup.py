3afa465d303be4f5dc78067476befabf6d49954b
Tarcisio 'Kbça' Cantalice <tarcisiocantalice@gmail.com>
2020-02-11 16:13:37 -0300

    refactor(Setup): Remove invalid classifier

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Flask',
-        'License :: MIT',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
     ],
